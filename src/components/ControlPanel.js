968550ca8ee11c0f937afe667982e88ff6b18aa9
Carlin Mitchell <carlinmi@umn.edu>
2023-06-09 15:13:43 -0500

    “sync”

---
+++ src/components/ControlPanel.js
@@ -0,0 +1,8 @@
+const ControlPanel = () => {
+  const panelContainer = Object.assign(document.createElement("div"), {
+    className: "control-panel-container",
+  });
+  return panelContainer;
+};
+
+export default ControlPanel;
