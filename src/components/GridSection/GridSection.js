f0ff23eeed19d7b3e2e70a94518d7ee5aeffdf5c
Carlin Mitchell <carlinmi@umn.edu>
2023-06-13 08:54:13 -0500

    “conrol overlap working”

--- src/components/GridSection/GridSection.js
+++ src/components/GridSection/GridSection.js
@@ -1,16 +1,16 @@
 import ControlIcon from "./ControlIcon/ControlIcon";
 
-const Grid = () => {
+const GridSection = () => {
   const container = Object.assign(document.createElement("div"), {
     id: "grid-container",
   });
 
   const grid = Object.assign(document.createElement("div"), { id: "grid" });
 
-  // container.appendChild(ControlIcon());
+  container.appendChild(ControlIcon());
   container.appendChild(grid);
 
   return container;
 };
 
-export default Grid;
+export default GridSection;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,8 @@ import GridSection from "./GridSection/GridSection";
 
 const Main = () => {
   const main = Object.assign(document.createElement("main"), {});
-
+  const div = document.createElement("div");
+  div.id = "test";
   main.appendChild(ControlPanel());
   main.appendChild(GridSection());
 
